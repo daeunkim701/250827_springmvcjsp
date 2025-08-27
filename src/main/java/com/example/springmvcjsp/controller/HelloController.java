b8c1dbff89f6049c22569ba771fb8e94c944d8bf
daeunkim701 <lionboot2025@gmail.com>
2025-08-27 16:38:36 +0900

    수정

--- src/main/java/com/example/springmvcjsp/controller/HelloController.java
+++ src/main/java/com/example/springmvcjsp/controller/HelloController.java
@@ -1,13 +1,17 @@
 package com.example.springmvcjsp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 // @RequestGetMapping("/") // DispatcherServlet 어쩌구저쩌구
 public class HelloController {
     @GetMapping("/greeting")
-    public String greet() {
+    public String greet(Model model) {
+        // key, value
+        model.addAttribute("username", "김다은");
+        model.addAttribute("message", "아자차카타파하");
         return "greet";
         // web-app/WEB-INF/views/ + {...} + .jsp
     }
